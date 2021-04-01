b085816f3ae5e8999c78d7e51b2aff4f344b464a
vnguyen@nexthealthtechnologies.com <vnguyen@nexthealthtechnologies.com>
2021-03-31 19:57:02 -0600

    wish list

--- src/flaskapp/api/resources/wish_list.py
+++ src/flaskapp/api/resources/wish_list.py
@@ -1,72 +1,182 @@
-import json
 import logging
-from functools import wraps
 
 from flask import jsonify, request
 from flask_restful import Resource
-from flaskapp.api.models.books import Book
-from flaskapp.extensions import db  # db is sqlalchemy db management
-from sqlalchemy.sql import \
-    text  # for text queries.  We're not doing much with the ORM for microservices, we use
+from flaskapp.api.models.wish_list import WishList
+from flaskapp.extensions import db
+from sqlalchemy import exc
 
 from .error import validate_error
-
-# from .helper import get_properties_query, validate
+from .helper import is_book, is_user, validate, validate_user_id
 
 
 class WishListResource(Resource):
     def get(self):
         """
+        Get book wish list
+        ---
+        parameters:
+        responses:
+          200:
+            description: Return list of books
+          404:
+            description: No book available
+        """
+        result = WishList.query.all()
+        if not result:
+            return validate_error("None books available in wish list", 404)
+        response = jsonify([row.to_dict() for row in result])
+
+        return response
+
+    def post(self):
+        """
         Add book to wish list
         ---
         parameters:
           - in: path
-            name: start_date
+            name: isbn
             schema:
-                type: datetime(year, month, day)
+                type: string
             required: true
-            description: start date
+            description: isbn of the book
           - in: path
-            name: num_of_nights
+            name: user_id
             schema:
                 type: int
             required: true
-            description: number of nights
+            description: user id
         responses:
-          200:
-            description: Return list of properties and descriptions
-          401:
-            description: Unauthorized request
+          201:
+            description: Add book to wish list
           400:
             description: Invalid request param
           404:
-            description: No properties available
+            description: Either user_id or isbn doesnt exists
         """
-        result = Book.query.all()
-        response = jsonify(json_list=result)
-        return response
+        isbn = request.args.get("isbn", None)
+        user_id = request.args.get("user_id", None)
+        errors = validate(isbn, user_id)
+        if errors:
+            logging.warning(errors)
+            return validate_error(errors, 400)
 
-        # start_date = request.args.get("start_date", None)
-        # num_of_nights = request.args.get("num_of_nights", None)
-        # errors = validate(start_date, num_of_nights)
-        # if errors:
-        #     logging.warning(errors)
-        #     return validate_error(errors, 400)
-        # return get_properties(start_date, num_of_nights)
+        if not is_book(isbn):
+            msg = f"isbn  - {isbn} doesnt exist"
+            logging.warning(msg)
+            return validate_error(msg, 404)
 
+        if not is_user(user_id):
+            msg = f"user_id - {user_id} doesnt exist"
+            logging.warning(msg)
+            return validate_error(msg, 404)
 
-# def get_properties(request_start_date, request_num_of_nights):
-#     query = get_properties_query(request_start_date, request_num_of_nights)
-#     logging.info(query)
-#     result = db.get_engine().execute(text(query)).fetchall()
-#     if not result:
-#         return validate_error(
-#             f"No property available in giving range {request_start_date} - {request_num_of_nights}",
-#             404,
-#         )
+        try:
+            result = WishList(isbn=isbn, user_id=user_id)
+            db.session.add(result)
+            db.session.commit()
+        except exc.IntegrityError:
+            msg = f"Fail to insert into db"
+            return validate_error(msg, 400)
+        return "Added", 201
 
-#     logging.info(f"There are {len(result)} properties")
+    def put(self):
+        """
+        Update book wish list
+        ---
+        parameters:
+          - in: path
+            name: old_isbn
+            schema:
+                type: string
+            required: true
+            description: isbn of the book to be update
+          - in: path
+            name: new_isbn
+            schema:
+                type: string
+            required: true
+            description: isbn of the new book
+          - in: path
+            name: user_id
+            schema:
+                type: int
+            required: true
+            description: user id
+        responses:
+          201:
+            description: Update book to wish list
+          400:
+            description: Invalid request param
+          404:
+            description: Either user_id or isbn doesnt exists
+        """
+        old_isbn = request.args.get("old_isbn", None)
+        new_isbn = request.args.get("new_isbn", None)
+        user_id = request.args.get("user_id", None)
+        error = validate_user_id(user_id)
+        if error:
+            logging.warning(error)
+            return validate_error(error, 400)
 
-#     response = jsonify([dict(row) for row in result])
-#     response.status_code = 200
-#     return response
+        if not is_book(old_isbn) or not is_book(new_isbn):
+            return validate_error(
+                f"Either old or new isbn {old_isbn} - {new_isbn} doesnt exist", 404
+            )
+
+        result = WishList.query.filter(WishList.isbn==old_isbn, WishList.user_id==user_id).first()
+        if not result:
+            result = WishList(isbn=new_isbn, user_id=user_id)
+        try:
+            result.isbn = new_isbn
+            db.session.commit()
+        except exc.IntegrityError:
+            return validate_error(f"Fail to insert into db", 400)
+
+        return "Updated", 200
+
+    def delete(self):
+        """
+        Delete book from wish list
+        ---
+        parameters:
+          - in: path
+            name: isbn
+            schema:
+                type: string
+            required: true
+            description: isbn of the book
+          - in: path
+            name: user_id
+            schema:
+                type: int
+            required: true
+            description: user id
+        responses:
+          200:
+            description: Add book to wish list
+          400:
+            description: Invalid request param
+          404:
+            description: Either user_id or isbn doesnt exists
+        """
+        isbn = request.args.get("isbn", None)
+        user_id = request.args.get("user_id", None)
+        error = validate_user_id(user_id)
+        if error:
+            logging.warning(error)
+            return validate_error(error, 400)
+
+        if isbn and isbn != "None" and not is_book(isbn):
+            return validate_error(f"isbn  - {isbn} doesnt exist", 404)
+
+        if not is_user(user_id):
+            return validate_error(f"user_id - {user_id} doesnt exist", 404)
+
+        try:
+            WishList.query.filter_by(**request.args).delete()
+            db.session.commit()
+        except exc.IntegrityError:
+            return validate_error(f"Fail to delete {request.args}", 400)
+
+        return "Deleted", 200
